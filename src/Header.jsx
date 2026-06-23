364abab02ac0e718781d748994c36a3624f2eae2
Gabriel André <ggmatias2@gmail.com>
2026-06-23 02:01:46 -0300

    feat: add hero section

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+export const Header = () => {
+  return (
+    <header className="header-menu">
+      <div className="logo">
+        <a href="#">Buenas Viagens!</a>
+      </div>
+
+      <nav className="header-menu-nav">
+        <a href="#">Home</a>
+        <a href="#">Passeios</a>
+        <a href="#">Destinos</a>
+        <a href="#">Avaliações</a>
+      </nav>
+    </header>
+  );
+};
---
+++ src/Hero.jsx
@@ -0,0 +1,34 @@
+import hero from "./assets/image-1.jpg";
+
+export const Hero = () => {
+  return (
+    <section className="hero">
+      <img className="hero-img" src={hero} alt="hero" />
+
+      <div className="content">
+        <h1 className="title">EXPLORE O MUNDO!</h1>
+
+        <div className="search-box-wrapper">
+          <div className="search-box">
+            <div className="field">
+              <label>Para onde vamos?</label>
+              <input type="text" />
+            </div>
+
+            <div className="field">
+              <label>Quando?</label>
+              <input type="date" />
+            </div>
+
+            <div className="field">
+              <label>Retorno</label>
+              <input type="date" />
+            </div>
+
+            <button>Buscar</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
